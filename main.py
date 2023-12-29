e72e432b71d8d510e4a2830cea5af7048092a5ca
pramodyasahan <sahandil1013@gmail.com>
2023-12-29 19:06:09 +0530

    chore: simplify the code

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 inputs = [1, 2, 3, 2.5]
-weights1 = [0.2, 0.8, -0.5, 1.0]
-weights2 = [0.5, -0.91, 0.26, -0.5]
-weights3 = [-0.26, -0.27, 0.17, 0.87]
+weights = [[0.2, 0.8, -0.5, 1.0], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]]
+biases = [2, 3, 0.5]
 
-bias1 = 2
-bias2 = 3
-bias3 = 0.5
+layer_outputs = []
+for neuron_weight, neuron_bias in zip(weights, biases):
+    neuron_output = 0
+    for n_inputs, n_weights in zip(inputs, neuron_weight):
+        neuron_output += n_inputs * n_weights
+    neuron_output += neuron_bias
+    layer_outputs.append(neuron_output)
 
-output = [weights1[0] * inputs[0] + weights1[1] * inputs[1] + weights1[2] * inputs[2] + weights1[3] * inputs[3] + bias1,
-          weights2[0] * inputs[0] + weights2[1] * inputs[1] + weights2[2] * inputs[2] + weights2[3] * inputs[3] + bias2,
-          weights3[0] * inputs[0] + weights3[1] * inputs[1] + weights3[2] * inputs[2] + weights3[3] * inputs[3] + bias3]
-print(output)
+print(layer_outputs)
