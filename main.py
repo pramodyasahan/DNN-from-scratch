b1472696b43c4226f05190593553a1a6d3752948
pramodyasahan <sahandil1013@gmail.com>
2023-12-30 20:40:51 +0530

    feat: add a new layer

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-inputs = [1, 2, 3, 2.5]
-weights = [[0.2, 0.8, -0.5, 1.0], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]]
-biases = [2, 3, 0.5]
+import numpy as np
 
-layer_outputs = []
-for neuron_weight, neuron_bias in zip(weights, biases):
-    neuron_output = 0
-    for n_inputs, n_weights in zip(inputs, neuron_weight):
-        neuron_output += n_inputs * n_weights
-    neuron_output += neuron_bias
-    layer_outputs.append(neuron_output)
+inputs = [[1, 2, 3, 2.5],
+          [2.0, 5.0, -1.0, -0.8],
+          [-1.5, 2.7, 3.3, -0.8]]
 
-print(layer_outputs)
+weights1 = [[0.2, 0.8, -0.5, 1.0],
+            [0.5, -0.91, 0.26, -0.5],
+            [-0.26, -0.27, 0.17, 0.87]]
+
+biases1 = [2, 3, 0.5]
+
+weights2 = [[0.1, -0.14, 0.5],
+            [-0.5, 0.12, -0.33],
+            [-0.44, 0.73, -0.13]]
+
+biases2 = [-1, 2, -0.5]
+
+layer = np.dot(inputs, np.array(weights1).T) + biases1
+print(layer)
