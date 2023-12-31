cc37be38895cbfd68c6f2966bf60f8fd3ce569dd
pramodyasahan <sahandil1013@gmail.com>
2023-12-31 15:05:16 +0530

    feat: implement softmax activation

--- main.py
+++ main.py
@@ -20,14 +20,8 @@ class ActivationReLU:
         self.output = np.maximum(0, inputs)
 
 
-X = [[1, 2, 3, 2.5],
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
-
-X, y = spiral_data(100, 3)
-
-layer1 = LayerDense(2, 5)
-activation1 = ActivationReLU()
-layer1.forward(X)
-activation1.forward(layer1.output)
-print(activation1.output)
+class ActivationSoftmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
